997118d4b581f1832672d43ecc31cc7a868804e7
333Nikita333 <nikitosfox333@gmail.com>
2023-02-20 00:15:08 +0200

    finished task 02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Contacts from './Contacts';
+import Form from './Form';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  createContact = (name, number) => {
+    return { id: nanoid(4), name, number };
+  };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value });
+    console.log(this.state);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const name = form.elements.name.value;
+    const tel = form.elements.number.value;
+
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, this.createContact(name, tel)],
+    }));
+
+    console.log(this.state.contacts);
+    form.reset();
+  };
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <section>
+        <h1>Phonebook</h1>
+        <Form onSubmit={this.handleSubmit} onChange={this.handleChange} />
+        <Contacts contactsTitle={'Contacts'} contacts={contacts} />
+      </section>
+    );
+  }
+}
